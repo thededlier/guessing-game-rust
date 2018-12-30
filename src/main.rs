5804328557e215c0664f8226f456bd918e9ec782
Rohan Anand <thededlier@gmail.com>
2018-12-30 20:18:49 +0530

    getting and matching with random number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 11);
+    let mut guess = String::new();
+
     println!("Guess the number");
     println!("Please enter your guess");
-    let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed {}", guess);
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+    println!("The secret number is: {}", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too large!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
